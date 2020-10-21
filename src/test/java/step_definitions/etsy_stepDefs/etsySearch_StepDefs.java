c1da78df96b1678feb6326534d6cb6cab16fe5cc
QAAnton <qa.antonsemenov@gmail.com>
2020-10-20 23:19:32 -0500

    updates

--- src/test/java/step_definitions/etsy_stepDefs/etsySearch_StepDefs.java
+++ src/test/java/step_definitions/etsy_stepDefs/etsySearch_StepDefs.java
@@ -3,10 +3,15 @@ package step_definitions.etsy_stepDefs;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.etsy_pages.Etsy_HomePage;
 import utilities.Config;
 import utilities.Driver;
 
 public class etsySearch_StepDefs {
+
+    Etsy_HomePage etsy_homePage = new Etsy_HomePage();
 
     @Given("user is on Etsy home page")
     public void user_is_on_Etsy_home_page() {
@@ -15,11 +20,14 @@ public class etsySearch_StepDefs {
 
     @When("user searches for wooden spoon")
     public void user_searches_for_wooden_spoon() {
-
+        etsy_homePage.searchBox.sendKeys("wooden spoon" + Keys.ENTER);
     }
 
     @Then("user should see wooden spoon in Etsy title")
     public void user_should_see_wooden_spoon_in_Etsy_title() {
+        String expectedInTitle = "wooden spoon";
+        String actualInTitle = Driver.getDriver().getTitle();
 
+        Assert.assertTrue(actualInTitle.contains(expectedInTitle));
     }
 }
