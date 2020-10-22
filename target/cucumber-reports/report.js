9c5cfd86d318fac8a5b029b32460b5b62c437291
Antonsemenov87 <Anthony04061987@gmail.com>
2020-10-22 00:11:05 -0500

    Background

--- target/cucumber-reports/report.js
+++ target/cucumber-reports/report.js
@@ -4,15 +4,10 @@ formatter.feature({
   "description": "  Agile story:\n  Jira #CBT-25",
   "keyword": "Feature"
 });
-formatter.scenario({
-  "name": "Etsy search title verification",
+formatter.background({
+  "name": "user is on Etsy home page",
   "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@etsy"
-    }
-  ]
+  "keyword": "Background"
 });
 formatter.before({
   "status": "passed"
@@ -26,6 +21,16 @@ formatter.match({
 });
 formatter.result({
   "status": "passed"
+});
+formatter.scenario({
+  "name": "Etsy search title verification",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@etsy"
+    }
+  ]
 });
 formatter.step({
   "name": "user searches for \"wooden spoon\"",
@@ -39,52 +44,6 @@ formatter.result({
 });
 formatter.step({
   "name": "user should see \"Wooden spoon\" in Etsy title",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "etsySearch_StepDefs.user_should_see_in_Etsy_title(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Etsy search title verification",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@etsy2"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user is on Etsy home page",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "etsySearch_StepDefs.user_is_on_Etsy_home_page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user searches for \"custom wooden spoon\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "etsySearch_StepDefs.user_searches_for(String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user should see \"Custom wooden spoon\" in Etsy title",
   "keyword": "Then "
 });
 formatter.match({
--- target/test-classes/features/etsy_features/etsySearching.feature
+++ target/test-classes/features/etsy_features/etsySearching.feature
@@ -2,20 +2,21 @@ Feature: Etsy search functionality
   Agile story:
   Jira #CBT-25
 
+Background: user is on Etsy home page
+  Given user is on Etsy home page
+
 
   Scenario: Etsy search title verification
-    Given user is on Etsy home page
     When user searches for wooden spoon
     Then user should see wooden spoon in Etsy title
 
   @etsy
   Scenario: Etsy search title verification
-    Given user is on Etsy home page
     When user searches for "wooden spoon"
     Then user should see "Wooden spoon" in Etsy title
 
-    @etsy2
+
   Scenario: Etsy search title verification
-    Given user is on Etsy home page
     When user searches for "custom wooden spoon"
     Then user should see "Custom wooden spoon" in Etsy title
+
